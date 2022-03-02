2769029640ea5c202b81a157bbca73e23cdbdf7e
KBogam <80017179+KBogam@users.noreply.github.com>
2022-03-01 19:32:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function change(){
+    document.getElementById("text").style.backgroundColor="yellow";
+    document.getElementById("text").style.color="black";
+    
+}
+
+
+function changeafter(){
+    document.getElementById("text").style.backgroundColor="rgb(68, 92, 160)";
+    document.getElementById("text").style.color="white";
+}
